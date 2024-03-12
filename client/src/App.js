20dbe834e43581ad643a2b5e58affbb7e6715b91
chernetGit <henok.gebresenbet@gmail.com>
2024-03-12 10:46:25 +0200

    setting up frontend

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer />
+      <h1>hompage</h1>
+    </div>
+  );
+}
+
+export default App;
